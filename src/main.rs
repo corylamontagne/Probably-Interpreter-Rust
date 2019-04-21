9e8b9a7bd28d586d0d0ef2b6ac10a75958fa0efe
corylamontagne <corylamontagne@yahoo.com>
2019-04-20 22:17:42 -0400

    Open a prob file and output its contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "test.prob";
+    match File::open(filename) {
+            // The file is open (no error).
+            Ok(mut file) => {
+                let mut content = String::new();
+
+                // Read all the file content into a variable (ignoring the result of the operation).
+                file.read_to_string(&mut content).unwrap();
+
+                println!("{}", content);
+
+                // The file is automatically closed when is goes out of scope.
+            },
+            // Error handling.
+            Err(error) => {
+                println!("Error opening file {}: {}", filename, error);
+            },
+        }
 }
